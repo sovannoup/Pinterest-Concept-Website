a622ae2fc72997428cff884b4af25de532e5fc43
SOVAN NOUP <sovannoup>
2021-07-26 10:42:39 +0700

    First Initial

---
+++ routes/authors.js
@@ -0,0 +1,129 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+const Book = require('../models/book')
+
+// All Authors Route
+router.get('/', async (req, res) => {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    })
+  } catch {
+    res.redirect('/')
+  }
+})
+// render login
+router.get('/login', (req, res) => {
+  res.render('authors/login')
+})
+
+//login Author Route
+router.post('/login', async (req, res) => {
+  try{
+    const author = await Author.findOne({email: req.body.email })
+    if (!author || author.password != req.body.password){
+      res.render('authors/new', {
+        errorMessage: 'Incorrect Email or Password'
+    })}
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Login Successfully'
+    })
+  }catch{
+    res.redirect('/')
+  }
+})
+
+// New Author Route
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() })
+})
+
+// Create Author Route
+router.post('/', async (req, res) => {
+  if (req.body.password != req.body.confirmpassword)
+    {
+      res.render('authors/new', {
+        errorMessage: 'Passwords should be the same'
+      })
+    }
+  const author = await new Author({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  })
+  try {
+    const newAuthor = await author.save()
+    res.redirect(`authors/${newAuthor.id}`)
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author'
+    })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    const books = await Book.find({ author: author.id }).limit(6).exec()
+    res.render('authors/show', {
+      author: author,
+      booksByAuthor: books
+    })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    res.render('authors/edit', { author: author })
+  } catch {
+    res.redirect('/authors')
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    author.name = req.body.name
+    await author.save()
+    res.redirect(`/authors/${author.id}`)
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.render('authors/edit', {
+        author: author,
+        errorMessage: 'Error updating Author'
+      })
+    }
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    await author.remove()
+    res.redirect('/authors')
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.redirect(`/authors/${author.id}`)
+    }
+  }
+})
+
+module.exports = router
